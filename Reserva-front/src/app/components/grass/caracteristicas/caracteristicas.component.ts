db2923d9ecf75745387678f60e2cb44d6c871638
Dino Roca <rocalaurented@gmail.com>
2023-11-07 22:08:11 -0500

    Plantilla de caracteristicas lado GRASS 2.0

--- Reserva-front/src/app/components/grass/caracteristicas/caracteristicas.component.ts
+++ Reserva-front/src/app/components/grass/caracteristicas/caracteristicas.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-caracteristicas',
   templateUrl: './caracteristicas.component.html',
   styleUrls: ['./caracteristicas.component.css']
 })
-export class CaracteristicasComponent {
+export class CaracteristicasComponent implements OnInit {
 
+  public ilumination = false;
+  public techado = false;
+  public garaje = false;
+  public canchafut: Number = 0;
+  public canchavol: Number = 0;
+
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _title: Title,
+    private _toastrService: ToastrService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this._title.setTitle('GRASS | características');
+  }
 }
