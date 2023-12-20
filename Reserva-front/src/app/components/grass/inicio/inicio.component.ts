04039595c5bae679257e6ed804c4ba1d4d430223
Vladimir <vladimir.sarmiento.27@unsch.edu.pe>
2023-12-19 20:47:43 -0500

    Diseño V1: Grass/Inicio
    Actualización del diseño en "cambio de portada del grass"

--- Reserva-front/src/app/components/grass/inicio/inicio.component.ts
+++ Reserva-front/src/app/components/grass/inicio/inicio.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { UserService } from 'src/app/services/user.service';
-import { GLOBAL } from '../../../services/global';
 import { v4 as uuidv4 } from 'uuid';
+import { GLOBAL } from '../../../services/global';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
   public empresa: any = {};
@@ -23,14 +23,16 @@ export class InicioComponent implements OnInit {
   public load_btn_eliminar = false;
   public file: File | any = undefined;
 
+  imagePreview: string | ArrayBuffer | null = null;
+
   constructor(
     private _route: ActivatedRoute,
     private _userService: UserService,
     private _title: Title,
     private _toastrService: ToastrService
   ) {
-
-    this.token = localStorage.getItem('token') || sessionStorage.getItem('token');
+    this.token =
+      localStorage.getItem('token') || sessionStorage.getItem('token');
     this.id = localStorage.getItem('_id') || sessionStorage.getItem('_id');
     this.url = GLOBAL.url;
 
@@ -39,8 +41,9 @@ export class InicioComponent implements OnInit {
 
   init_data() {
     this.load_data = true;
-    this._userService.obtener_empresa(this.id, this.token).subscribe(
-      response => {
+    this._userService
+      .obtener_empresa(this.id, this.token)
+      .subscribe((response) => {
         if (response.data == undefined) {
           this.empresa = undefined;
           this.load_data = false;
@@ -62,14 +65,13 @@ export class InicioComponent implements OnInit {
 
           this.load_data = false;
         }
-      }
-    );
+      });
   }
 
   ngOnInit(): void {
     this._title.setTitle('GRASS | Galería de canchas');
   }
-
+  //--------------------------------------------------------------
   fileChangeEvent(event: any): void {
     const file = event.target.files && event.target.files[0];
 
@@ -84,7 +86,13 @@ export class InicioComponent implements OnInit {
     }
 
     if (
-      !['image/png', 'image/webp', 'image/jpg', 'image/jpeg', 'image/gif'].includes(file.type)
+      ![
+        'image/png',
+        'image/webp',
+        'image/jpg',
+        'image/jpeg',
+        'image/gif',
+      ].includes(file.type)
     ) {
       this.showErrorMessage('El archivo debe ser una imagen');
       return;
@@ -94,6 +102,23 @@ export class InicioComponent implements OnInit {
     reader.readAsDataURL(file);
 
     this.file = file;
+
+    if (file) {
+      // Convierte el archivo en una URL de datos (data URL)
+      this.convertToBase64(file);
+    }
+  }
+
+  convertToBase64(file: File): void {
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      // Asigna la URL de datos a la propiedad imagePreview
+      this.imagePreview = e.target.result;
+    };
+
+    // Lee el contenido del archivo como una URL de datos
+    reader.readAsDataURL(file);
   }
 
   subir_imagen() {
@@ -103,17 +128,17 @@ export class InicioComponent implements OnInit {
     if (this.file) {
       const data = {
         imagen: this.file,
-        _id: uuid
+        _id: uuid,
       };
 
-      this._userService.agregar_imagen_portada(this.id, data, this.token).subscribe(
-        response => {
-          this._toastrService.success('Se subió con éxito', 'SUBIDO!');
+      this._userService
+        .agregar_imagen_portada(this.id, data, this.token)
+        .subscribe((response) => {
+          this._toastrService.success('Se subió con éxito', '¡SUBIDO!');
           this.init_data();
           this.file = undefined;
           this.load_btn = false;
-        }
-      );
+        });
     } else {
       this.showErrorMessage('Debe seleccionar una imagen');
       this.load_btn = false;
@@ -122,13 +147,13 @@ export class InicioComponent implements OnInit {
 
   eliminar(id: any) {
     this.load_btn_eliminar = true;
-    this._userService.eliminar_imagen_portada(this.id, { _id: id }, this.token).subscribe(
-      response => {
+    this._userService
+      .eliminar_imagen_portada(this.id, { _id: id }, this.token)
+      .subscribe((response) => {
         this._toastrService.success('Se eliminó con éxito', 'ELIMINADO!');
         this.load_btn_eliminar = false;
         this.init_data();
-      }
-    );
+      });
   }
 
   private showErrorMessage(message: string) {
